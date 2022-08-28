cd70af4470a5d3e64f3e0d61aa3abc9457833519
mfurkankarakulak <furkan-karakulak@hotmail.com>
2022-08-28 16:16:44 +0300

    class structure

---
+++ ClassStructure/src/str/HelloWorld.java
@@ -0,0 +1,15 @@
+package str;
+
+/**
+ * 
+ * @author Mustafa Furkan Karakulak
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
